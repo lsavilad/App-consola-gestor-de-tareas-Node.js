327ca1acb1c8f797faef851b9992e79b31f52dc3
Luis Avila <luisavila@titamedia.com>
2022-02-16 21:36:25 -0500

    Aplicacion de consola

---
+++ app.js
@@ -0,0 +1,68 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu,
+    pausa,
+    leerInput,
+    listadoTareasBorrar,
+    confirmar,
+    mostrarListadoChecklist
+} = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+
+console.clear();
+const main = async () => {
+
+    let opt = '';
+    const tareas = new Tareas();
+    const tareasDB = leerDB();
+    if (tareasDB) {
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    do {
+
+        opt = await inquirerMenu();
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                tareas.listadoCompleto();
+                //console.log(tareas.listadoArr);
+                break;
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+                break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+                break;
+            case '5':
+                const ids = await mostrarListadoChecklist(tareas.listadoArr);
+                tareas.toggleCompletadas(ids);
+                break;
+            case '6':
+
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== '0') {
+                    const ok = await confirmar('¿Esta Seguro?');
+                    if (ok) {
+                        tareas.borrarTarea(id);
+                        console.log('Tarea borrada')
+                    }
+                }
+                break;
+            case '0':
+                //crear opcion
+                break;
+
+
+        }
+        guardarDB(tareas.listadoArr);
+        await pausa();
+
+    } while (opt !== '0');
+
+}
+main();
---
+++ models/tareas.js
@@ -0,0 +1,86 @@
+const Tarea = require("./tarea");
+
+class Tareas {
+
+    _listado = {};
+
+    get listadoArr() {
+        const listado = [];
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        });
+        return listado;
+    }
+
+    constructor() {
+        this._listado = {};
+    }
+
+    borrarTarea(id = '') {
+        if (this._listado[id]) {
+            delete this._listado[id];
+        }
+    }
+
+    cargarTareasFromArray(tareas = []) {
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        })
+        this._listado
+    }
+
+    crearTarea(desc = '') {
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+    }
+
+    listadoCompleto() {
+        console.log();
+        this.listadoArr.forEach((tarea, i) => {
+            const id = i + 1;
+            const { desc, completadoEn } = tarea;
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red
+            console.log(`${id} ${desc} :: ${estado} `)
+        })
+
+    }
+
+    listarPendientesCompletadas(completadas = true) {
+        console.log();
+        let id = 0;
+        this.listadoArr.forEach((tarea, i) => {
+            const { desc, completadoEn } = tarea;
+            const estado = (completadoEn) ? 'Completada'.green : 'Pendiente'.red
+            if (completadas) {
+                if (completadoEn) {
+                    id += 1;
+                    console.log(`${(id + '.').green} ${desc} :: ${completadoEn.green} `)
+                }
+            } else {
+                if (!completadoEn) {
+                    id += 1;
+                    console.log(`${(id + '.').green} ${desc} :: ${estado} `)
+                }
+            }
+        })
+    }
+
+    toggleCompletadas(ids = []) {
+        ids.forEach(id => {
+            const tarea = this._listado[id];
+            if(!tarea.completadoEn){
+                tarea.completadoEn = new Date().toISOString();
+            }
+        })
+
+        this.listadoArr.forEach(tarea =>{
+            if (!ids.includes(tarea.id)){
+                this._listado[tarea.id].completadoEn = null;
+            }
+        })
+    }
+
+}
+
+module.exports = Tareas;
